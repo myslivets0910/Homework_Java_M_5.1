6f4b3003a616398b1ac19de0d2d30519254fe1c2
pavlik_masl_QA <122479950+myslivets0910@users.noreply.github.com>
2023-04-23 13:21:28 +0300

    Update BonusServiceMain.java

--- src/main/java/BonusServiceMain.java
+++ src/main/java/BonusServiceMain.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BonusServiceMain {
+public class BonusServiceMain {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long bonus = service.calculate(5_000, true);
+        System.out.println(bonus);
+
+    }
 }
